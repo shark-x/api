22ef3668f396b2d8ae9025095a42c0fb0b26618e
Anastasia <anastasiya-karpeeva@smartworld.team>
2021-05-27 10:06:28 +0700

    add first test

---
+++ spec/test/first.test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert
+
+describe('First simple test-suite', function(){
+
+    it('first test-case', function(){
+    
+        assert(2+2, 4, 'Сообщение об ошибке')
+        
+    })
+})
